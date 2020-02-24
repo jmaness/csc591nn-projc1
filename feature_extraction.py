9ba48e13bf344c566c8532088fc9fa629ce5f316
npool9 <npool@elon.edu>
2020-02-24 15:40:17 -0500

    Train and validate SVM classifier

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
 from skimage.feature import hog
+from sklearn import svm
+from sklearn.model_selection import train_test_split
 
 
 class Main:
@@ -105,46 +107,85 @@ class Main:
                                   cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         return key_points, image
 
-    def extract_hog(self, array):
+    def extract_hog(self, array, visualize=False):
         """
         Get the Histogram of Gradients of the image
         :param array: a numpy array (image)
+        :param visualize: to visualize the HOG image or not [True, False]
         :return: HOG vector (for training), HOG image (for visualization)
         """
-        feature_vector, im = hog(array, orientations=8,
-                                    pixels_per_cell=(4, 4),
-                                    cells_per_block=(1, 1),
-                                    block_norm='L2-Hys',
-                                    feature_vector=True,
-                                    visualize=True)
-        return feature_vector, im
+        if visualize:
+            vector, im = hog(array,
+                             orientations=8,
+                             pixels_per_cell=(4, 4),
+                             cells_per_block=(1, 1),
+                             block_norm='L2-Hys',
+                             feature_vector=True,
+                             visualize=True)
+            return vector, im
+        else:
+            vector = hog(array,
+                         orientations=8,
+                         pixels_per_cell=(4, 4),
+                         cells_per_block=(1, 1),
+                         feature_vector=True,
+                         block_norm='L2-Hys')
+            return vector
 
 
 if __name__ == "__main__":
     main = Main()
     X, y, filenames = main.load_data()
+
     # preview random datapoint
-    index = random.randint(0, len(X))
-    print("Previewing image", filenames[index], "from class", y[index])
-    main.visualize_data(X[index])
+    # index = random.randint(0, len(X))
+    # print("Previewing image", filenames[index], "from class", y[index])
+    # main.visualize_data(X[index])
 
     # Edge feature extraction and visualization
-    edge_image = main.edge_filter(X[index], f_type='canny')
-    main.visualize_data(edge_image)
+    # edge_image = main.edge_filter(X[index], f_type='canny')
+    # main.visualize_data(edge_image)
 
     # Extract channel and visualization
-    green_channel = main.extract_channel(X[index], channel='green')
-    main.visualize_data(green_channel)
+    # green_channel = main.extract_channel(X[index], channel='green')
+    # main.visualize_data(green_channel)
 
     # Extract SIFT features of image and visualization
-    points, point_image = main.extract_sift(X[index])
-    print("SIFT Key Points:", points)
-    main.visualize_data(point_image)
+    # points, point_image = main.extract_sift(X[index])
+    # print("SIFT Key Points:", points)
+    # main.visualize_data(point_image)
 
     # Extract HOG features of image and visualization
-    vector, image = main.extract_hog(X[index])
-    print("HOG Feature Descriptor:", vector)
-    main.visualize_data(image)
+    # vector, image = main.extract_hog(X[index])
+    # print("HOG Feature Descriptor:", vector)
+    # main.visualize_data(image)
 
-    print(X[index].flatten().shape)
-    print(np.ravel(X[index]).shape)
+    # Prepare data with concatenation of features of your choice
+    # These are flattened to vectors
+    print("Extracting features...")
+    for i in range(len(X)):
+        if i % 10 == 0:
+            print("Preprocessing Image", i+1, '/', len(X))
+        hog_vector = main.extract_hog(X[i], visualize=False)
+        green = main.extract_channel(X[i], channel="green")
+        X[i] = np.concatenate((hog_vector.flatten(), green.flatten()), axis=0)
+    print("Vector size:", X[0].shape)
+
+    # Split the data
+    print("Splitting the data...")
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y,
+                                                          train_size=0.8,
+                                                          random_state=138,
+                                                          shuffle=True,
+                                                          stratify=y)
+
+    # Train the model on the prepared data
+    print("Training the model...")
+    clf = svm.SVC()
+    clf.fit(X_train, y_train)
+
+    # Predict
+    print("Validating the model...")
+    y_hat = clf.predict(X_valid)
+    accuracy = sum(y_valid == y_hat) / len(y_valid)
+    print("Accuracy:", accuracy)
